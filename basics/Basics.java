96db872dec36a6275c9b490958216f915a052ae0
Sarah Fisher <sajafisher@gmail.com>
2019-09-17 11:59:27 -0700

    clock and stretch goal completed

--- basics/Basics.java
+++ basics/Basics.java
@@ -1,14 +1,13 @@
-// import java.text.SimpleDateFormat;
-// import java.util.Calendar;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 public class Basics {
 
   public static void main(String[] args) {
-    // Calendar cal = Calendar.getInstance();
-    // System.out.println(new SimpleDateFormat("MMMM").format(cal.getTime()));
     System.out.println(pluralize("dog", 3));
     System.out.println(flipNHeads(4));
+    clock();
   }
 
   public static String pluralize(String word, int number) {
@@ -38,4 +37,25 @@ public class Basics {
     } while (count != number);
     return "It took " + flips + " flips to flip " + count + " heads in a row.";
   }
+
+  //write a method clock that uses Java's built-in LocalDateTime object to constantly print out the current time to the console by second. The program should run until killed with CTRL-C or 'stop' button in IDE. Print once per second. Program should detect time change to print. Use zero-pad single digit numbers so the timestamp characters length stays the same.
+
+  public static String clock() {
+    LocalDateTime now = LocalDateTime.now();
+    int oldSeconds = now.getSecond();
+    String time = now.format(DateTimeFormatter.ofPattern(("HH:mm:ss")));
+    int count = 0;
+    do {
+      now = LocalDateTime.now();
+      int newSeconds = now.getSecond();
+      count++;
+       if (newSeconds != oldSeconds) {
+        time = now.format(DateTimeFormatter.ofPattern(("HH:mm:ss")));
+        System.out.println(time + " " + (count/1_000) + "kHz");
+        oldSeconds = newSeconds;
+        count = 0;
+       }
+    } while (true);
+  }
+
 }
