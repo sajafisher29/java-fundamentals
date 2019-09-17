36e272f3a559871be8afb5c8ce78861f5de5e8d6
Sarah Fisher <sajafisher@gmail.com>
2019-09-17 12:08:54 -0700

    Added comments about lab sections

--- basics/Basics.java
+++ basics/Basics.java
@@ -2,6 +2,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
+//Lab 1 Java Primitives and Control Flow for Java 401
+// Used resources https://docs.oracle.com/javase/8/docs/api/java/util/Random.html and https://docs.oracle.com/javase/8/docs/api/java/time/LocalDateTime.html
+//Focus on Java’s built-in primitive values (ints, floats, doubles, Strings, booleans), for loops, while loops, if statements, functions, parameters and return statements
+
 public class Basics {
 
   public static void main(String[] args) {
@@ -9,6 +13,8 @@ public class Basics {
     System.out.println(flipNHeads(4));
     clock();
   }
+
+  //Write a function called pluralize that accepts a word and a number and returns a string with the word pluralized with an “s” if the number is zero, or greater than one.
 
   public static String pluralize(String word, int number) {
     if (number == 0 || number > 1) {
@@ -18,6 +24,8 @@ public class Basics {
       }
   }
 
+  //Write a function called flipNHeads that accepts an integer n and flips coins until n heads are flipped in a row. Simulate coin flipping by choosing a random number between 0 and 1. Numbers below .5 are considered tails. Numbers at and above .5 are considered heads. Print out heads or tails on one line for each flip. Print It took FLIPS flips to flip N heads in a row. once n heads have been in a row.
+  
   public static String flipNHeads(int number) {
     Random random = new Random();
     int count = 0;
