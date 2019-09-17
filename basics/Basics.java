0486a740c0fcf1743999ce8f787cf789d2ade1ce
Sarah Fisher <sajafisher@gmail.com>
2019-09-17 11:02:56 -0700

    second section of lab completed

--- basics/Basics.java
+++ basics/Basics.java
@@ -1,5 +1,6 @@
 // import java.text.SimpleDateFormat;
 // import java.util.Calendar;
+import java.util.Random;
 
 public class Basics {
 
@@ -7,6 +8,7 @@ public class Basics {
     // Calendar cal = Calendar.getInstance();
     // System.out.println(new SimpleDateFormat("MMMM").format(cal.getTime()));
     System.out.println(pluralize("dog", 3));
+    System.out.println(flipNHeads(4));
   }
 
   public static String pluralize(String word, int number) {
@@ -17,4 +19,23 @@ public class Basics {
       }
   }
 
+  public static String flipNHeads(int number) {
+    Random random = new Random();
+    int count = 0;
+    int flips = 0;
+    do {
+      double randomNumber = random.nextDouble();
+      if (randomNumber > 0 && randomNumber < .5) {
+      System.out.println("tails");
+      count = 0;
+      flips++;
+      System.out.println(randomNumber);
+      } else if (randomNumber > .5 && randomNumber < 1){
+      System.out.println("heads");
+      count++;
+      flips++;
+      }
+    } while (count != number);
+    return "It took " + flips + " flips to flip " + count + " heads in a row.";
+  }
 }
