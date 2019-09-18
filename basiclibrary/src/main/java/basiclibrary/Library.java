edc148b8c3ca51d2bb34159aec1c703fc7bcfcb6
Sarah Fisher <sajafisher@gmail.com>
2019-09-18 09:44:21 -0700

    first draft tests written

--- basiclibrary/src/main/java/basiclibrary/Library.java
+++ basiclibrary/src/main/java/basiclibrary/Library.java
@@ -7,14 +7,21 @@ public class Library {
     public boolean someLibraryMethod() {
         return true;
     }
+
+    //Write a method called roll that accepts an integer n and rolls a six-sided dice n times. The method should return an array containing the values of the rolls.
+    public void testRoll
+
+    //Write a method called containsDuplicates that returns true or false depending on whether the array contains duplicate values.
+    public void testContainsDuplicates
+
+    //Write a method that accepts an array of integers and calculates and returns the average of all the values in the array.
+    public void testCalculatingAverages
+
+    //Given an array of arrays calculate the average value for each array and return the array with the lowest average.
+    public void testLowestAverageArr
+
 }
 
-//Write a method called roll that accepts an integer n and rolls a six-sided dice n times. The method should return an array containing the values of the rolls.
 
-
-//Write a method called containsDuplicates that returns true or false depending on whether the array contains duplicate values.
-
-
-//Given an array of arrays calculate the average value for each array and return the array with the lowest average.
 
 
