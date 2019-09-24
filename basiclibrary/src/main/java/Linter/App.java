805574c2aefcc052afde69734af80723e504ad2a
Sarah Fisher <sajafisher@gmail.com>
2019-09-24 07:38:59 -0700

    Starting to build the Linter section

--- basiclibrary/src/main/java/Linter/App.java
+++ basiclibrary/src/main/java/Linter/App.java
@@ -1,39 +1,23 @@
-package basiclibrary;
+package Linter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
-public class Linter {
+public class App {
 
     //Analyzing Weather Data: Using the October Seattle weather data below, iterate through all of the data to find the min and max values. Use a HashSet of type Integer to keep track of all the unique temperatures seen. Iterate from the min temp to the max temp and create a String containing any temperature not seen during the month. Return that String.
-
-    //Data: Daily average temperatures for Seattle, October 1-28 2017
-    //int[][] weeklyMonthTemperatures = {
-    //  {66, 64, 58, 65, 71, 57, 60},
-    //  {57, 65, 65, 70, 72, 65, 51},
-    //  {55, 54, 60, 53, 59, 57, 61},
-    //  {65, 56, 55, 52, 55, 62, 57}
-    //};
-
-    //Expected output:
-    //High: 72
-    //Low: 51
-    //Never saw temperature: 63
-    //Never saw temperature: 67
-    //Never saw temperature: 68
-    //Never saw temperature: 69
 
     //Iterate through the data to find the min and max values
     //Use an int HashSet to track all unique temperatures in the data set
     //Iterate from the min to max temp and create a String containing any temperature not seen during the month
 
     public static String analyzeWeatherData(int[][] weeklyMonthTemperatures){
+        HashSet<Integer> trackWeatherData = new HashSet<>();
         int minTemperature = Integer.MAX_VALUE;
         int maxTemperature = Integer.MIN_VALUE;
         StringBuilder returnDataString = new StringBuilder();
-
-        HashSet<Integer> trackWeatherData = new HashSet<>();
 
         for (int i = 0; i < weeklyMonthTemperatures.length; i++) {
             for (int j = 0; j < weeklyMonthTemperatures[i].length; j++) {
@@ -59,51 +43,37 @@ public class Linter {
     }
 
     //Tallying Election: Write a function called tally that accepts a List of Strings representing votes and returns one string to show what got the most votes.
-    //Within main method include
-    //List<String> votes = new ArrayList<>();
-    //votes.add("Bush");
-    //votes.add("Bush");
-    //votes.add("Bush");
-    //votes.add("Shrub");
-    //votes.add("Hedge");
-    //votes.add("Shrub");
-    //votes.add("Bush");
-    //votes.add("Hedge");
-    //votes.add("Bush");
-    //String winner = tally(votes);
-    //System.out.println(winner + " received the most votes!");
 
     public static String tally(List<String> votesList) {
-        votesList = new ArrayList<>();
-        votesList.add("Bush");
-        votesList.add("Bush");
-        votesList.add("Bush");
-        votesList.add("Shrub");
-        votesList.add("Hedge");
-        votesList.add("Shrub");
-        votesList.add("Bush");
-        votesList.add("Hedge");
-        votesList.add("Bush");
-        String winner = tally(votesList);
-//        return winner + " received the most votes!";
+        HashMap<String, Integer> voteCount = new HashMap<>();
 
-//        HashMap<String, Integer> tally = new HashMap<>();
-//
-//        for (String vote : votesList) {
-//            if (votesList.containsKey(vote)) {
-//                int previousVotes = votesList.get(vote);
-//                votesList.put(vote, previousVotes + 1);
-//            } else {
-//                votesList.put(vote, 1);
-//            }
-//        }
-//        String winnerVote = votes.get(0);
-//        for (String vote : votesList.keySet()) {
-//            if(votesList.get(vote) > votesList.get(winnerVote)) {
-//                winnerVote = vote;
-//            }
-//        }
-//        return winnerVote;
-//    }
-//}
+        //If there are no votes
+        if (votesList.size() == 0) {
+            return "There were no votes recorded.";
+        }
+
+        //Tally the votes per candidate and store totals in voteCount
+        for (String vote : votesList) {
+            if (voteCount.containsKey(vote)) {
+                voteCount.replace(vote, voteCount.get(vote) + 1);
+            } else {
+                voteCount.put(vote, 1);
+            }
+        }
+
+        //Identify who has the highest vote tally and story name in electionWinner
+        //For a tie, concat the winners
+        int highestTally = 0;
+        String electionWinner = "";
+
+        for (String vote : voteCount.keySet()) {
+            if (voteCount.get(vote) > highestTally) {
+                electionWinner = vote;
+                highestTally = voteCount.get(vote);
+            } else if (voteCount.get(vote) == highestTally) {
+                electionWinner = electionWinner + " and " + vote;
+            }
+        }
+        return electionWinner;
+    }
 }
---
+++ basiclibrary/src/test/java/Linter/LinterTest.java
@@ -0,0 +1,7 @@
+package Linter;
+
+import static org.junit.Assert.*;
+
+public class LinterTest {
+
+}
